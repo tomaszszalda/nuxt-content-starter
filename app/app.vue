657e700070307a5b579e6c4a601a0d02f16a9120
Tomasz Szałda <tomek.szalda@gmail.com>
2025-10-02 14:58:11 +0200

    update demo

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,4 @@
 <template>
-  <NuxtRouteAnnouncer />
+  <!-- <NuxtRouteAnnouncer /> -->
   <NuxtPage />
 </template>
--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 const route = useRoute()
 console.log("LAYER")
-const { data: page } = await useAsyncData('page-' + route.path, () => {
-  return queryCollection('content').path(route.params.slug).first()
+const { data: page } = await useAsyncData(  () => {
+  
+  return queryCollection('content').path('/'+route.params.slug[0]).count()
 })
-console.log("LAYER", page.value, route.params.slug)
-if (!page.value) {
-  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
-}
+console.log("LAYER", page.value, route.params.slug[0])
+// if (!page.value) {
+//   throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
+// }
 </script>
 
 <template>
